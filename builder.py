c5a79a76f95b85e563fa3a5bddb9fe4f93243606
az13js <1654602334@qq.com>
2019-06-04 11:28:14 +0800

    生成了5000条假数据模仿股市变动,重新生成结果

--- builder.py
+++ builder.py
@@ -14,10 +14,13 @@ def build(file, lines = 3, columns = 4):
     for i in range(lines - 1):
         line_data = []
         for j in range(columns):
-            line_data.append(float(data[i][j] + 0.5 * np.random.normal(0, 1, 1)))
+            if 0 == j:
+                line_data.append(float(data[i][j] + 0.5 * np.random.normal(0, 1, 1)))
+            else:
+                line_data.append(float(data[i][0] + 0.05 * np.random.normal(0, 1, 1)))
         data.append(line_data)
     data_frame = pd.DataFrame(data)
     data_frame.to_csv(file)
 
 if __name__ == "__main__":
-    build('stock_data.csv', 100, 6)
+    build('stock_data.csv', 5000, 6)
