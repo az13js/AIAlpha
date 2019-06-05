0facac2903927ee8dbb9ae3f3af3030ff764f9ba
az13js <1654602334@qq.com>
2019-06-05 12:35:24 +0800

    分析拆分过程

--- README.md
+++ README.md
@@ -21,3 +21,13 @@
 按照上面几个脚本的顺序执行可以重现实验结果：
 
 ![](result.png)
+
+## 文件
+
+`stock_data.csv`：训练用数据。`preprocessing/indicators.csv`，未经过降噪的数据后半段相对于前半段变化的幅度。`preprocessing/log_train.csv`降噪后的数据，这个降噪是单独对拆分的小数据片段进行降噪，而不是在整个数据集上统一进行的降噪。`preprocessing/rbm_train.csv`和`preprocessing/rbm_test.csv`都是从`preprocessing/log_train.csv`中拆一部分出来的。`preprocessing/test_data.csv`原始数据里面拿小片段第5列最后一个数据，跟倒数前一个数据差分对数后的结果。`preprocessing/test_stock.csv`直接取原始数据第5列。`stock_data_test.csv`从`preprocessing/test_stock.csv`里拆开的一部分。
+
+编码器是使用`preprocessing/rbm_train.csv`训练的。训练完成后对`preprocessing/log_train.csv`进行编码，保存为`features/autoencoded_data.csv`。
+
+然后从`features/autoencoded_data.csv`中拆分出`features/autoencoded_test_data.csv`和`features/autoencoded_train_data.csv`。再从`preprocessing/test_data.csv`中拆分`features/autoencoded_train_y.csv`和`features/autoencoded_test_y.csv`。
+
+LSTM 的训练是以`features/autoencoded_train_data.csv`作为输入，`features/autoencoded_train_y.csv`作为输出进行的。训练完成后有根据`features/autoencoded_test_data.csv`和`features/autoencoded_test_y.csv`评估拟合程度。数据的预测上，
---
+++ test_predict.py
@@ -0,0 +1,156 @@
+# -*- coding: UTF-8 -*-
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from bokeh.plotting import output_file, figure, show
+
+
+class NeuralNetwork:
+    def __init__(self, input_shape, stock_or_return):
+        self.input_shape = input_shape
+        self.stock_or_return = stock_or_return
+
+    def create_model(self, input_shape):
+        # 这个必须是一个整数
+        # input_shape = 20
+
+        # 原始实现是用 Keras 框架做的，这里除了特殊指定之外，其余参数用 Keras 默认的参数配置
+        model = tf.keras.models.Sequential()
+        # 输入层
+        model.add(tf.keras.layers.InputLayer(input_shape=(1, input_shape)))
+        # 第一层 LSTM
+        model.add(tf.keras.layers.LSTM(
+            units=5, # 指定为5
+            activation='tanh',
+            recurrent_activation='hard_sigmoid',
+            use_bias=True,
+            kernel_initializer='glorot_uniform',
+            recurrent_initializer='orthogonal',
+            bias_initializer='zeros',
+            unit_forget_bias=True,
+            kernel_regularizer=None,
+            recurrent_regularizer=tf.keras.regularizers.l2(l=0), # 指定为0
+            bias_regularizer=None,
+            activity_regularizer=tf.keras.regularizers.l2(l=0.003), # 指定为0.003
+            kernel_constraint=None,
+            recurrent_constraint=None,
+            bias_constraint=None,
+            dropout=0.2, # 指定为0.2
+            recurrent_dropout=0.2, # 指定为0.2
+            implementation=1,
+            return_sequences=True, # 指定为False
+            return_state=False,
+            go_backwards=False,
+            stateful=False,
+            unroll=False
+        ))
+        # Dense 层
+        model.add(tf.keras.layers.Dense(
+            units=5, # 指定为5
+            activation='sigmoid', # 指定为sigmoid
+            use_bias=True,
+            kernel_initializer='glorot_uniform',
+            bias_initializer='zeros',
+            kernel_regularizer=None,
+            bias_regularizer=None,
+            activity_regularizer=tf.keras.regularizers.l2(l=0.005), # 指定为0.005
+            kernel_constraint=None,
+            bias_constraint=None
+        ))
+        # 第二层 LSTM
+        model.add(tf.keras.layers.LSTM(
+            units=2, # 指定为2
+            activation='tanh',
+            recurrent_activation='hard_sigmoid',
+            use_bias=True,
+            kernel_initializer='glorot_uniform',
+            recurrent_initializer='orthogonal',
+            bias_initializer='zeros',
+            unit_forget_bias=True,
+            kernel_regularizer=None,
+            recurrent_regularizer=tf.keras.regularizers.l2(l=0.001), # 指定为0.001
+            bias_regularizer=None,
+            activity_regularizer=tf.keras.regularizers.l2(l=0.01), # 指定为0.003
+            kernel_constraint=None,
+            recurrent_constraint=None,
+            bias_constraint=None,
+            dropout=0.2, # 论文指定为0.2
+            recurrent_dropout=0.2, # 论文指定为0.2
+            implementation=1,
+            return_sequences=False,
+            return_state=False,
+            go_backwards=False,
+            stateful=False,
+            unroll=False
+        ))
+        # Dense 层
+        model.add(tf.keras.layers.Dense(
+            units=1, # 指定为1
+            activation='sigmoid', # 指定为sigmoid
+            use_bias=True,
+            kernel_initializer='glorot_uniform',
+            bias_initializer='zeros',
+            kernel_regularizer=None,
+            bias_regularizer=None,
+            activity_regularizer=tf.keras.regularizers.l2(l=0.001), # 指定为0.005
+            kernel_constraint=None,
+            bias_constraint=None
+        ))
+        return model
+
+    def make_train_model(self):
+        model = self.create_model(self.input_shape)
+        model.compile(optimizer="adam", loss="mean_squared_error", metrics=["mse"])
+
+        # load data
+
+        train = np.reshape(np.array(pd.read_csv("features/autoencoded_train_data.csv", index_col=0)),
+                           (len(np.array(pd.read_csv("features/autoencoded_train_data.csv"))), 1, self.input_shape))
+        train_y = np.array(pd.read_csv("features/autoencoded_train_y.csv", index_col=0))
+        # train_stock = np.array(pd.read_csv("train_stock.csv"))
+
+        # train model
+
+        model.fit(train, train_y, epochs=2000)
+
+        test_x = np.reshape(np.array(pd.read_csv("features/autoencoded_test_data.csv", index_col=0)),
+                            (len(np.array(pd.read_csv("features/autoencoded_test_data.csv"))), 1, self.input_shape))
+        test_y = np.array(pd.read_csv("features/autoencoded_test_y.csv", index_col=0))
+        # test_stock = np.array(pd.read_csv("test_stock.csv"))
+
+        stock_data_test = np.array(pd.read_csv("stock_data_test.csv", index_col=0))
+
+        print(model.evaluate(test_x, test_y))
+        prediction_data = []
+        stock_data = []
+        for i in range(len(test_y)):
+            prediction = (model.predict(np.reshape(test_x[i], (1, 1, self.input_shape))))
+            prediction_data.append(np.reshape(prediction, (1,)))
+            std = np.std(prediction_data)
+            if 0 == std:
+                std = 0.0001
+            prediction_corrected = (np.array(prediction_data) - np.mean(prediction_data)) * (1.0 / std)
+            stock_price = np.exp(np.reshape(prediction, (1,)))*stock_data_test[i]
+            stock_data.append(stock_price[0])
+        stock_data[:] = [i - (float(stock_data[0])-float(stock_data_test[0])) for i in stock_data]
+        # stock_data = stock_data - stock_data[0]
+        if self.stock_or_return:
+            plt.plot(stock_data)
+            plt.plot(stock_data_test)
+            stock = pd.DataFrame(stock_data, index=None)
+            stock_test = pd.DataFrame(stock_data_test, index=None)
+            # print(stock_data)
+            plt.show()
+        else:
+            # plt.plot(prediction_corrected)
+            plt.plot(prediction_data)
+            # print(prediction_data)
+            plt.plot(test_y)
+            plt.show()
+
+
+if __name__ == "__main__":
+    model = NeuralNetwork(20, False)
+    model.make_train_model()
